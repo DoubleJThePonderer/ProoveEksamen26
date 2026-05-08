fff377068f42d6bb5e27e86e262f7d83802a2c02
DoubleJ <johannesoriginal@gmail.com>
2026-05-08 12:33:05 +0200

    fra start til 12:32 prøveeksamen

--- app.py
+++ app.py
@@ -7,10 +7,10 @@ app.secret_key = '28ku3fl8Gq17'
 
 def get_dbconection():
     return mysql.connector.connect(
-    host="",
-    user="",
+    host="10.2.1.233",
+    user="remoteuser",
     password="IMIKUB",
-    database=""
+    database="spillforum"
     )
 
 @app.route('/')
@@ -22,7 +22,7 @@ def login():
         username = request.form['username']
         password = request.form['password']
         cursor = mydb.cursor()
-        cursor.execute('SELECT * FROM accounts WHERE username = %s AND password =%s', (username, password))
+        cursor.execute('SELECT * FROM users WHERE username = %s AND password =%s', (username, password))
         account = cursor.fetchone()
         if account:
             session['loggedin'] = True
@@ -32,3 +32,44 @@ def login():
         else:
             msg='Incorrect username/password'
     return render_template('login.html', msg=msg)
+
+@app.route('/logout')
+def logout():
+    session.pop('loggedin', None)
+    session.pop('id', None)
+    session.pop('username', None)
+    return redirect(url_for('login'))
+
+@app.route('/register', methods=['GET', 'POST'])
+def register():
+    mydb = get_dbconection()
+    msg=''
+    if request.method == 'POST' and 'username' in request.form and 'password' in request.form and 'email' in request.form:
+        username = request.form['username']
+        password = request.form['password']
+        email = request.form['email']
+        cursor = mydb.cursor()
+        cursor.execute('SELECT * FROM users WHERE username = %s', (username,))
+        account = cursor.fetchone()
+        if account:
+            msg = 'account already exists'
+        elif not re.match(r'[^@]+@[^@]+\.[^@]+', email):
+            msg = 'invalid email address'
+        elif not re.match(r'[A-Za-z0-9]+', username):
+            msg = 'Username must contain only letters and numbers'
+        elif not username or not password or not email:
+            msg = 'Please fill out the form'
+        else:
+            cursor.execute('INSERT INTO users VALUES(NULL, %s, %s, %s)', (username, password, email))
+            mydb.commit()
+            cursor.execute('SELECT * FROM users WHERE username = %s AND password =%s', (username, password))
+            account = cursor.fetchone()
+            session['loggedin'] = True
+            session['id'] = account[0]
+            session['username'] = account[1]
+            return render_template('index.html', msg = 'you have successfully registerd')
+            
+    return render_template('register.html', msg=msg)
+
+if __name__ == '__main__':
+    app.run(debug=True)
---
+++ logg.md
@@ -0,0 +1,27 @@
+## 11:43
+    cursor.execute('SELECT * FROM users WHERE username = %s AND password =%s', (username, password))
+    account = cursor.fetchone()
+    session['loggedin'] = True
+    session['id'] = account[0]
+    session['username'] = account[1]
+    return render_template('index.html', msg = 'you have successfully registerd')
+-det var det jeg skrev  til å skape inlogging da du har registrert
+
+## 12:31
+    <header>
+        {% if session['loggedin'] == True: %}
+        <h1>{{  session.username }}</h1>
+        <input type="text">
+        <a href="{{ url_for('logout') }}">log out</a>
+        {% else %}
+        <h1>logged out</h1>
+        <input type="text">
+        <a href="{{ url_for('login') }}">log in</a>
+        {% endif %}
+    </header>
+- lagde en top bar for index intill videre
+
+## 
+
+##  kilder
+- https://pythonexamples.org/python-flask-if-statement-in-html-template/
